5565178d9a2dc73333558aa5ab84a73801baa43c
Ryan Riddle <ryanriddle@berkeley.edu>
2016-01-26 23:23:50 +0000

    almost done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,53 @@
 # Part 1
 
 def sum arr
-  return 0
+  sum = 0
+  arr.each { |a| sum+=a }
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr = arr.sort
+    return arr[-1] + arr [-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 1
+      return false
+  elsif arr.length == 0
+    if n == 0
+      return true
+    else
+      return false
+    end
+  else
+    return true
+    # look thriough all permitations of array
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name.to_str
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[01]*00$/
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
